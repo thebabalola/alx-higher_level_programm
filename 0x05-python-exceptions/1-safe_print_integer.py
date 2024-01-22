453a90ac39dfc4dcacb2857b33db9510cf15a83c
thebabalola <t.babalolajoseph@gmail.com>
2024-01-22 18:10:02 +0100

    python exception commit

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def safe_print_integer(value):
+    """Print an integer with "{:d}".format().
+
+    Args:
+        value (int): The integer to print.
+
+    Returns:
+        True if successful, False otherwise.
+    """
+    try:
+        print("{:d}".format(value))
+        return True
+
+    except (TypeError, ValueError):
+        return False
